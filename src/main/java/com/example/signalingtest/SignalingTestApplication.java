1fda26f7f64b38bd2ca48737381372b588e26b9a
freeftr <jonghaparkdevelop@gmail.com>
2025-01-08 14:51:27 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/com/example/signalingtest/SignalingTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.signalingtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignalingTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SignalingTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/signalingtest/SignalingTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.signalingtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignalingTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
