f1ea4e82e8a1b03f49bd1929dca8903a02fbd0b0
freeftr <jonghaparkdevelop@gmail.com>
2025-01-08 15:27:46 +0900

    fix: bug fix

--- src/main/java/com/example/signalingtest/config/WebSocketConfiguration.java
+++ src/main/java/com/example/signalingtest/config/WebSocketConfiguration.java
@@ -4,19 +4,22 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfiguration{
+public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
 
-    public void configureMessageBroker (MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic"); //broker url 설정
-        config.setApplicationDestinationPrefixes("/app"); //send url 설정
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic"); // Broker URL 설정
+        config.setApplicationDestinationPrefixes("/app"); // Send URL 설정
     }
 
-    public void registerStompEndPoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/signaling") // webSocket 접속시 endpont 설정
-                .setAllowedOrigins("*") // cors에 따른 설정
-                .withSockJS(); // 브라우저에서 WebSocket을 지원하지 않는 경우에 대안으로 애플리케이션의 코드를 변경할 필요 없이 런타임에 필요할 때 대체하기 위해 설정
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/signaling") // WebSocket 접속시 엔드포인트 설정
+                .setAllowedOrigins("*") // CORS 설정
+                .withSockJS(); // 브라우저에서 WebSocket을 지원하지 않는 경우에 대비한 설정
     }
 }
